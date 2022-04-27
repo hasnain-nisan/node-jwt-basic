e7dc7442f3c52704b842b38097cf1c7900c5b9b4
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 <hasnain.nisan@outlook.com>
2022-04-27 12:02:39 +0600

    middleware and server setup

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('server started');
+console.log('JWT basic in node');
+
+
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express')
+const app = express()
+
+//middleware
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+
+//json_middleware
+app.use(express.json())
+
+
+//Frontend view route
+app.get('/', (req, res) => {
+    res.send(`<div style="display:flex; align-items:center; flex-direction:column; text-decoration:underline">
+        <h1 >JWT basic in MERN</h1>
+    </div>`)
+})
+
+//port
+const port = process.env.PORT|| 5000
+const start = async () => {
+    try {
+        app.listen(port, console.log(`Server is listening at port: ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
